7a5a0880929437045723f138afa66447c134aba1
Maximilian Rolf Kleiner <kleinerm@fide.de>
2024-01-27 10:12:13 +0100

    added noisier flashes

--- Circus.gd
+++ Circus.gd
@@ -1,20 +1,12 @@
 extends Node2D
 
-@onready var color_sprite: Sprite2D = $'.'
-
-var timer:float = 0
+signal flashes(new_likelyhood:int)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("I am here... now!")
+	flashes.emit(20)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	timer+=delta
-	rotate(delta)
-	scale = scale + scale*20*delta
-	color_sprite.modulate.a-=delta
-	if timer>2 :
-		print("dead")
-		queue_free()
+	pass
--- Scripts/Effects/FlashInstancer.gd
+++ Scripts/Effects/FlashInstancer.gd
@@ -3,18 +3,26 @@ extends Node2D
 var likelyhood:int = 30
 var rng = RandomNumberGenerator.new()
 var timer:float = 0
+
+var randomTimer = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomTimer=rng.randf_range(-.3,1.0)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timer+=delta
-	if timer>1:
+	if timer>1+randomTimer:
 		timer=0
 		if rng.randi_range(1,100) < likelyhood:
 			var instance = preload("res://Assets/Flashes/basesflash.tscn").instantiate()
 			instance.position = position;
 			add_child(instance)
 	pass
+
+func set_likelyhood(new_likelyhood):
+	likelyhood = new_likelyhood
+	print(likelyhood)
+	pass # Replace with function body.
