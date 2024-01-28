983621d33449d798b8d727392d0b51d172ace153
Benjamin Kleiner <bizzl@benkle.info>
2024-01-28 10:33:36 +0100

    Fixed invisible juggler

--- Scripts/Acts.gd
+++ Scripts/Acts.gd
@@ -23,7 +23,9 @@ func _unicycle_button_pressed(_viewport, event, _shape_idx):
 
 func _cake_button_pressed(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		#$Juggling.do_hide()
+		if $Juggling.is_hidden():
+			$Juggling.add_5_balls()
+		$Juggling.do_show()
 		$Unicycle.visible = false
 		$Pie.visible = true
 		$Pie.fling_da_pie()
