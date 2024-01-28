a6ee148cd5aacdc1b41489a40626b78fcfd67c48
Benjamin Kleiner <bizzl@benkle.info>
2024-01-28 10:12:44 +0100

    Pies are flying!

---
+++ Scripts/Acts/Pie.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+signal pie_has_hit()
+signal pie_fling_finished()
+var pie_is_flying: bool = false
+
+func fling_da_pie():
+	$Pied.frame = 0
+	$Pied.stop()
+	$Pied.visible = false
+	$Pie/Follow.progress_ratio = 0
+	$Pie/Follow/Sprite.scale = Vector2(2, 2)
+	$Pie/Follow/Sprite.visible = true
+	pie_is_flying = true
+
+func _process(delta):
+	if pie_is_flying:
+		$Pie/Follow.progress_ratio += delta
+		var scale: int = 2 - 0.5 * $Pie/Follow.progress_ratio
+		$Pie/Follow/Sprite.scale = Vector2(scale, scale)
+		if $Pie/Follow.progress_ratio > 0.95:
+			splat()
+			
+func splat():
+	pie_is_flying = false
+	pie_has_hit.emit()
+	$Pied.visible = true
+	$Pie/Follow/Sprite.visible = false
+	$Pied.play('default')
+
+func _on_pied_animation_finished():
+	pie_fling_finished.emit()
